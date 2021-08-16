580088317a026b0713b16aafe5ee811fe3643991
MAngelFM <88851006+MAngelFM@users.noreply.github.com>
2021-08-16 11:24:39 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+     "name": "Área Triángulo",
+      "setup": "sudo -H pip3 install pytest",
+      "run": "pytest assignments/02AreaTriangulo",
+      "input": "",
+      "output": "",
+      "comparison": "included",
+      "timeout": 10,
+      "points": null
+        
     pass
 
 
